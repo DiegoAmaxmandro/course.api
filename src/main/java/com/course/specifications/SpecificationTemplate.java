d7f0140d507ee96e6a3f23bc0a716c305d3a9ca2
DiegoAmaxmandro <102663353+DiegoAmaxmandro@users.noreply.github.com>
2023-08-15 13:57:54 +0100

    Configuration and specifications

---
+++ src/main/java/com/course/specifications/SpecificationTemplate.java
@@ -0,0 +1,18 @@
+package com.course.specifications;
+
+import com.course.models.CourseModel;
+import net.kaczmarzyk.spring.data.jpa.domain.Equal;
+import net.kaczmarzyk.spring.data.jpa.domain.Like;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import org.springframework.data.jpa.domain.Specification;
+
+public class SpecificationTemplate {
+
+    @And({
+            @Spec(path = "courseLevel", spec = Equal.class),
+            @Spec(path = "name", spec = Like.class),
+            @Spec(path = "courseStatus", spec = Equal.class)
+    })
+    public interface CourseSpec extends Specification<CourseModel> {}
+}
